45b56140694526960d75453ad32e2dbf9f774aa8
kyahki <112995845+kyahki@users.noreply.github.com>
2024-03-29 16:11:53 +0800

    Update Main.java

--- Arithmetic/Main.java
+++ Arithmetic/Main.java
@@ -4,14 +4,13 @@ public class Main{
     public static void main(String[] args) {
         
         try {
-            // Not sureeeeee
-            // Arithmetic<Integer, Integer> arithmetic = new Arithmetic<>(20, 0);
-            // Arithmetic<Float, Double> arithmetic = new Arithmetic<>(20.1f, 20.5);
-            // Arithmetic<Double, Double> arithmetic = new Arithmetic<>(20.9, 20.5);
-            // Arithmetic<Integer, Integer> arithmetic = new Arithmetic<>(20, 20);
-            // Arithmetic<Float, Float> arithmetic = new Arithmetic<>(20.2f, 20.5f);
-            // Arithmetic<String, Double> arithmetic = new Arithmetic<>("20", 20.5);
-            Arithmetic<Integer, Double> arithmetic = new Arithmetic<>(10, 10.5);
+             // Arithmetic<Integer, Integer> arithmetic = new Arithmetic<>(10, 10);
+            // Arithmetic<Float, Float> arithmetic = new Arithmetic<>(60.2f, 60.5f);
+            // Arithmetic<String, Double> arithmetic = new Arithmetic<>("10", 10.5);
+            // Arithmetic<Integer, Integer> arithmetic = new Arithmetic<>(30, 0);
+            // Arithmetic<Float, Double> arithmetic = new Arithmetic<>(40.1f, 40.5);
+            // Arithmetic<Double, Double> arithmetic = new Arithmetic<>(30.9, 30.5);
+            Arithmetic<Integer, Double> arithmetic = new Arithmetic<>(20, 20.5);
             
             System.out.println("Addition: " + (Number) arithmetic.add());
             System.out.println("Subtraction: " + (Number) arithmetic.subtract());
